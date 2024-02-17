a1fffeea106eaec47a89d3957f64752afb208ee5
Dreadrith <valorinferno@gmail.com>
2024-02-17 15:15:42 +0200

    Move PluginInfo to main + Bump version

--- EasySyncPlugin.cs
+++ EasySyncPlugin.cs
@@ -4,17 +4,22 @@ using HarmonyLib;
 
 namespace EasySync
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+
+    [BepInPlugin(MOD_ID, MOD_NAME, MOD_VERSION)]
     [BepInDependency("io.github.CSync")]
     public class EasySyncPlugin : BaseUnityPlugin
     {
-        private static readonly Harmony _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        public const string MOD_ID = "EasySync";
+        public const string MOD_NAME = "EasySync";
+        public const string MOD_VERSION = "0.0.2";
+        
+        private static readonly Harmony _harmony = new Harmony(MOD_ID);
         public static EasySyncPlugin instance;
         private ManualLogSource logSource => Logger;
 
         private void Awake()
         {
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin {MOD_ID} is loaded!");
             instance = this;
             _harmony.PatchAll(typeof(Patches));
         }
